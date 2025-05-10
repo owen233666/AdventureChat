3b833c03a6c1ab907915bc7fcaa1142cb54d2867
owen233666 <2858084927@qq.com>
2025-05-10 22:10:53 +0800

    EnderChestShow壳子

--- src/main/java/cn/owen233666/adventurechat/utils/CommandExecutor/EnderChestShowExecutor.java
+++ src/main/java/cn/owen233666/adventurechat/utils/CommandExecutor/EnderChestShowExecutor.java
@@ -1,4 +1,9 @@
 package cn.owen233666.adventurechat.utils.CommandExecutor;
 
+import net.minecraft.commands.CommandSourceStack;
+
 public class EnderChestShowExecutor {
+    public static int execute(CommandSourceStack source, String uuid){
+        return 0;
+    }
 }
